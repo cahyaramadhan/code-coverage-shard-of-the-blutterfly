5dd4370586e9e341611849915716306e0b735c38
mini45452 <36650594+mini45452@users.noreply.github.com>
2020-05-04 23:14:06 +0700

    Update ValidasiHoles.py

--- pytest-cov/src/ValidasiHoles.py
+++ pytest-cov/src/ValidasiHoles.py
@@ -1,12 +1,13 @@
-def countHoles(sentence) :
-  jumlahLubang = 0
+class ValidasiHoles :
+  def countHoles(sentence) :
+    jumlahLubang = 0
 
-  for i in range(0, len(sentence)) :
-    if (sentence[i] == 'A' || sentence[i] == 'D' || 
-        sentence[i] == 'O' || sentence[i] == 'P' || 
-        sentence[i] == 'Q' || sentence[i] == 'R') :
-      jumlahLubang = jumlahLubang + 1
-    else if (sentence[i] == 'B')
-      jumlahLubang = jumlahLubang + 2
+    for i in range(0, len(sentence)) :
+      if (sentence[i] == 'A' || sentence[i] == 'D' || 
+          sentence[i] == 'O' || sentence[i] == 'P' || 
+          sentence[i] == 'Q' || sentence[i] == 'R') :
+        jumlahLubang = jumlahLubang + 1
+      else if (sentence[i] == 'B')
+        jumlahLubang = jumlahLubang + 2
 
-  return jumlahLubang
+    return jumlahLubang

9ae20048b6cec4aa62bf096f5da8d8590ea7bd9e
mini45452 <36650594+mini45452@users.noreply.github.com>
2020-05-04 23:12:26 +0700

    Update faktorial.py

--- pytest-cov/src/faktorial.py
+++ pytest-cov/src/faktorial.py
@@ -1,8 +1,9 @@
-def faktorial(n)
-  if (n < 0) :
-    return -1
-  else :
-    result = 1
-    for i in range(1, n + 1)
-      result = result * i
-    return result
+class faktorial :
+  def factorial(n) :
+    if (n < 0) :
+      return -1
+    else :
+      result = 1
+      for i in range(1, n + 1) :
+        result = result * i
+      return result
